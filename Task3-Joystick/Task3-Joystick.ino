9327c563ac87c5ed8c6601f7127ad1b6f0e02e54
Umair Abidi <56173806+umairabidi@users.noreply.github.com>
2022-10-01 12:02:02 -0400

    Update Task3-Joystick.ino

--- Task3-Joystick/Task3-Joystick.ino
+++ Task3-Joystick/Task3-Joystick.ino
@@ -20,14 +20,34 @@ void setup(){
 void loop(){
 	int xPos = analogRead(VRx);
 	int yPos = analogRead(VRy);
+	
+	/*
 	Serial.print("X position: ");
 	Serial.println(xPos);
 	Serial.print("Y position: ");
 	Serial.println(yPos);
-	
+	*/
+
+	// Servo position control
 	// Map the x position on the joystick
 	// from 0-1023	(ADC read value)
 	// to 0-180		(angle)
-	// The middle position of the joystick corresponds to
-	// the middle of the Servo's rotation (90°)
-	servo.write((int)xPos/5.685);
+	servo.write((int)xPos/5.68);
+
+	// Stepper Motor speed control
+	// Linearly ramps up speed with potentiometer reading
+	// Dead zone between 502 and 522
+	// Min speed is 0
+	// Max speed is 15
+	if (yPos >= 522){
+		stepper.setSpeed((int)15.0/(1023-522)*(yPos-522));
+		stepper.step(10);
+	}
+	else if (yPos <= 502){
+		stepper.setSpeed((int)(-15.0/502)*yPos+15);
+		stepper.step(-10);
+	}
+	else {
+		stepper.setSpeed(0);
+	}
+}
